eb6af84a54507a6111016a19e22a6cc078d2585f
siyeon1122si <ssiyeon1122@gmail.com>
2023-07-03 14:46:07 +0900

    test

--- MargeTest/Program.cs
+++ MargeTest/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MargeTest
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Ruler ruler = new Ruler(10);
+            ruler.Run();
+        }
+    }
+
+    public class Ruler
+    {
+        private const float ONE_INCH = 2.54F;
+        public int Centimeter { get; set; } = 0;
+        public float Inch
+        {
+            get { return Centimeter* ONE_INCH; }
+            private set {  Centimeter = (int) (value / ONE_INCH); }
+        }
+
+        public Ruler(int cmValue) { Centimeter=cmValue; }
+
+        public void Run()
+        {
+            Console.WriteLine($"{this.Centimeter} cm 는 {this.Inch} inch 입니다.");
         }
     }
 }
